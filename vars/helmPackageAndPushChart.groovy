4196906ad88c5c4a0b298239c3f290715f3e6e06
Julien Boubechtoula <julien.boubechtoula@gmail.com>
2020-01-17 20:48:41 +0100

    Update Helm

--- vars/helmPackageAndPushChart.groovy
+++ vars/helmPackageAndPushChart.groovy
@@ -2,11 +2,15 @@
 
 def call(String repositoryName, String repositoryUrl, String username, String password) {
 	container('helm') {
+		stage('Install Helm Plugin') {
+			sh "apk add git"
+			sh "helm plugin install https://github.com/chartmuseum/helm-push"
+		}
         stage('Package Chart') {      
 			sh "helm package ."
 		}
 		stage('Configure Helm') {      
-			sh "helm add repo ${repositoryName} ${repositoryUrl} --username ${username} --password"
+			sh "helm repo add ${repositoryName} ${repositoryUrl} --username ${username} --password ${password}"
 		}
         stage('Publish Chart') {      
 			sh "helm push . ${repositoryName}"
