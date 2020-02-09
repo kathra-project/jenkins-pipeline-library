f1f25e7371027549f4d8f7c4381e1670c35a8fd8
Julien Boubechtoula <julien.boubechtoula@gmail.com>
2020-02-09 20:33:23 +0100

    Helm Pipeline - Fix Helm Plugin Install

--- vars/helmPackageAndPushChart.groovy
+++ vars/helmPackageAndPushChart.groovy
@@ -3,8 +3,8 @@
 def call(String repositoryName, String repositoryUrl, String username, String password) {
 	container('helm') {
 		stage('Install Helm Plugin') {
-			sh "apk add git"
-			sh "helm plugin install https://github.com/chartmuseum/helm-push"
+			sh "apk add git curl"
+			sh "helm plugin list | grep push && echo 'Plugin push already installed' || helm plugin install https://github.com/chartmuseum/helm-push"
 		}
         stage('Package Chart') {      
 			sh "helm package ."
