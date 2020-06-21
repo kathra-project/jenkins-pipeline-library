bc34d7a16e97e7ed59030607b9768e0a77bddee0
Julien Boubechtoula <julien.boubechtoula@gmail.com>
2020-06-21 18:40:46 +0200

    Build from tags

--- vars/getVersion.groovy
+++ vars/getVersion.groovy
@@ -16,29 +16,8 @@ def call(body) {
   ]
 
   stage('Preparing new version...'){
-    
-    def shortGitCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()+"-${BUILD_NUMBER}"
-    
-    // RELEASE BRANCH
-    if(GIT_BRANCH.equals("master")){
-      version.dockerVersion = callConfig.projectVersion
-      version.binaryVersion = callConfig.projectVersion
-    }
-    // DEV BRANCH
-    else if(GIT_BRANCH.equals("dev") ){
-      version.dockerVersion = callConfig.projectVersion.replace("-SNAPSHOT", "") + "-" + shortGitCommit
-      version.binaryVersion = callConfig.projectVersion
-    }
-    // RC BRANCH
-    else if (GIT_BRANCH.toUpperCase().endsWith("-RC")){
-      version.dockerVersion = callConfig.projectVersion.replace("-RC-SNAPSHOT", "-RC") + "-" + shortGitCommit
-      version.binaryVersion = callConfig.projectVersion
-    }
-    // FEATURE BRANCH
-    else {
-      version.dockerVersion = callConfig.projectVersion.replace("-SNAPSHOT", "") + "-" + GIT_BRANCH + "-" + shortGitCommit
-      version.binaryVersion = callConfig.projectVersion.replace("-SNAPSHOT", "-" + GIT_BRANCH + "-SNAPSHOT")
-    }
+    version.dockerVersion = callConfig.projectVersion
+    version.binaryVersion = callConfig.projectVersion
   }
 
   return version
